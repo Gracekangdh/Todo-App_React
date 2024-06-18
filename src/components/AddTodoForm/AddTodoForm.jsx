50e3e1096a26c0a8bb40c877e334495ed06e716f
Gracekangdh <gracekangdh@gmail.com>
2024-06-18 19:51:49 +1000

    feat: Make Delete Button

--- src/components/AddTodoForm/AddTodoForm.jsx
+++ src/components/AddTodoForm/AddTodoForm.jsx
@@ -5,6 +5,9 @@ export default function AddTodoForm({ onAdd }) {
   const handleChange = (e) => setText(e.target.value);
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (text.trim().length === 0) {
+      return;
+    }
     onAdd({ id: "고유한 아이디", text, status: "active" });
     setText("");
   };
--- src/components/ToDoList/TodoList.jsx
+++ src/components/ToDoList/TodoList.jsx
@@ -1,16 +1,31 @@
 import React, { useState } from "react";
 import AddTodoForm from "../AddTodoForm/AddTodoForm";
+import Todo from "../Todo/Todo";
 
 export default function TodoList() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    { id: "123", text: "shopping", status: "active" },
+    { id: "124", text: "study", status: "active" },
+  ]);
   const handleAdd = (todo) => {
     setTodos([...todos, todo]);
+  };
+  const handleUpdate = (updated) => {
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  };
+  const handleDelete = (deleted) => {
+    setTodos(todos.filter((t) => t.id !== deleted.id));
   };
   return (
     <section>
       <ul>
-        {todos.map((item, idx) => (
-          <li key={idx}>{item.text}</li>
+        {todos.map((item) => (
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
         ))}
       </ul>
       <AddTodoForm onAdd={handleAdd} />
