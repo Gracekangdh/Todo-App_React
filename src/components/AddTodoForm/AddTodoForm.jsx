a2d321b111115d90f5995da20c3d45de5ba9624e
Gracekangdh <gracekangdh@gmail.com>
2024-06-19 22:31:12 +1000

    feat: styling Add Form

--- src/components/AddTodoForm/AddTodoForm.jsx
+++ src/components/AddTodoForm/AddTodoForm.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import styles from "./AddTodoForm.module.css";
 
 export default function AddTodoForm({ onAdd }) {
   const [text, setText] = useState("");
@@ -8,18 +10,19 @@ export default function AddTodoForm({ onAdd }) {
     if (text.trim().length === 0) {
       return;
     }
-    onAdd({ id: "고유한 아이디", text, status: "active" });
+    onAdd({ id: uuidv4(), text, status: "active" });
     setText("");
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <input
+        className={styles.input}
         type="text"
         placeholder="Add Todo"
         value={text}
         onChange={handleChange}
       />
-      <button>Add</button>
+      <button className={styles.button}>Add</button>
     </form>
   );
 }
